9754276b1b25e0dacf030d84f9d491e1a528fa31
Ntuthuko <ntuthukorocks@gmail.com>
2023-05-28 17:14:43 +0200

    impleents list of disclosure

---
+++ VoluntaryDisclosure.Application/Disclosures/DisclosureParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoluntaryDisclosure.Application.Disclosures
+{
+    public class DisclosureParams
+    {
+        public string? Name { get; set; }
+    }
+}
--- VoluntaryDisclosure.Application/Disclosures/List.cs
+++ VoluntaryDisclosure.Application/Disclosures/List.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoluntaryDisclosure.Application.Core;
+using VoluntaryDisclosure.Domain;
+using VoluntaryDisclosure.Persistance;
 
 namespace VoluntaryDisclosure.Application.Disclosures
 {
     public class List
     {
+        public class Query:IRequest<Result<List<Disclosure>>>
+        {
+            public DisclosureParams DisclosureParams { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<Disclosure>>>
+        {
+            private readonly VoluntaryDisclosureDataContext context;
+
+            public Handler(VoluntaryDisclosureDataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Result<List<Disclosure>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var search = request.DisclosureParams == null ? "" :
+                    request.DisclosureParams.Name;
+                var query = await context.Disclosures.Where(x =>
+                    string.IsNullOrEmpty(search)? true : (x.FullName.Contains(search) || x.Email.Contains(search))
+                    ).ToListAsync();
+                return Result<List<Disclosure>>.Success(query);
+            }
+        }
     }
 }
