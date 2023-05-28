90fd414369b731e82734be2419aa9167473d51aa
Ntuthuko <ntuthukorocks@gmail.com>
2023-05-28 11:38:22 +0200

    Adds Data context

---
+++ VoluntaryDisclosure.Persistance/VoluntaryDisclosureDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VoluntaryDisclosure.Domain;
+
+namespace VoluntaryDisclosure.Persistance
+{
+    public class VoluntaryDisclosureDataContext:DbContext
+    {
+        public DbSet<Disclosure> Disclosures { get; set; }
+    }
+}
