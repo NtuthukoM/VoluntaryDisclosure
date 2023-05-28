161a8794afa0758980723ced4b3666f5c7b17c73
Ntuthuko <ntuthukorocks@gmail.com>
2023-05-28 11:31:00 +0200

    Add project files.

---
+++ VoluntaryDisclosure.Domain/Disclosure.cs
@@ -0,0 +1,14 @@
+﻿namespace VoluntaryDisclosure.Domain
+{
+    public class Disclosure
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool CriminalRecord { get; set; }
+        public bool RedsBankingDbListed { get; set; }
+        public bool NegativeCreditListing { get; set; }
+        public bool PerformanceManagementPlaced { get; set; }
+        public bool EthicalIndustringRestriction { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
